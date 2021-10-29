316f94f44b95b768dce7270ae919257695e013ac
YuriyFedosov <92025361+YuriyFedosov@users.noreply.github.com>
2021-10-29 22:25:44 +0300

    rev2

--- TestApp2/Program.cs
+++ TestApp2/Program.cs
@@ -14,7 +14,35 @@ namespace TestApp2
             /*Программно создайте текстовый файл и запишите в него 10 случайных чисел. 
              * Затем программно откройте созданный файл, рассчитайте сумму чисел в нем, 
              * ответ выведите на консоль.*/
-        
+
+            Console.WriteLine("Введите имя файла для записи, без расширения:");
+            string fileName = Console.ReadLine(); //Считываем Имя файла
+            fileName = fileName + ".txt"; //Добавляем расширение
+
+            StreamWriter writer = new StreamWriter(fileName); //Создаем файд
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++) //Пишем случайные значения в файл
+            {
+                writer.Write(random.Next(0,100)+" ");
+            }
+            writer.Close();  // Сохраняем
+
+            StreamReader reader = new StreamReader(fileName); //Начинаем читаль файл
+            string str = reader.ReadToEnd(); //перегнали строку в память
+            str = str.Trim();
+            //Console.WriteLine("Итого в файл записаны следующие данные: {0}", str); //Можно разлочить для контроля
+
+            string[] arr = str.Split(' '); //Считываем данные в массив
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++) //Подсчет суммы
+            {
+                sum = sum + Convert.ToInt32(arr[i]);
+            }
+
+            Console.WriteLine("Сумма записанных в файл данных равна: {0}", sum);
+            Console.ReadKey();
         }
     }
 }
